6992be66dc1d84e89b09c53961abdf71b70b4996
abhixen07 <118633703+abhixen07@users.noreply.github.com>
2025-04-22 05:46:30 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// Simple Comparision
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//Prefer to have same datatypres for comparision
+// console.log("2" > 1);
+// console.log("2" > 8);
+// console.log("02" > 1);
+
+// Comparision and equality check are two different things
+//Avoid these comparision
+//Prefer Clean Code
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === is strict converaion w.r.t datatypes
+
+console.log("2" == 2); //No Strict ==
+console.log("2" === 2); //Strict ===
